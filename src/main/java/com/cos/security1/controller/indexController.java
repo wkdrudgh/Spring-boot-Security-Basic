649cb59f3595797700b9d25891250bbcdeef6a2b
wkdrudgh <boo04178@naver.com>
2020-12-25 20:34:25 +0900

    시큐리티 설정

--- src/main/java/com/cos/security1/controller/indexController.java
+++ src/main/java/com/cos/security1/controller/indexController.java
@@ -1,7 +1,10 @@
 package com.cos.security1.controller;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller // view를 리턴
 public class indexController {
@@ -12,6 +15,36 @@ public class indexController {
 	public String index() {
 		// 머스테치 기본폴더 : src/main/resources/
 		// 뷰리졸버 설정 : templates(prefix), mustache(suffix) 생략가능	
-		return "index";
+		return "index"; // src/main/resources/templates/index.mustache
+	}
+	
+	@GetMapping("/user")
+	public @ResponseBody String user() {
+		return "user";
+	}
+	
+	@GetMapping("/admin")
+	public @ResponseBody String admin() {
+		return "admin";
+	}
+	
+	@GetMapping("/manager")
+	public @ResponseBody String manager() {
+		return "manager";
+	}
+	
+	@GetMapping("/login")
+	public @ResponseBody String login() {
+		return "login";
+	}
+	
+	@GetMapping("/join")
+	public @ResponseBody String join() {
+		return "join";
+	}
+	
+	@GetMapping("/joinProc")
+	public @ResponseBody String joinProc() {
+		return "회원가입 완료됨";
 	}
 }
