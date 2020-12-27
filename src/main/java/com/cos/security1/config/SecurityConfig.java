9c0b1f7ac527a2b7b317de3730c6500755f44676
wkdrudgh <boo04178@naver.com>
2020-12-27 19:28:53 +0900

    구글 로그인 준비

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -31,6 +31,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.formLogin()
 			.loginPage("/loginForm")
 			.loginProcessingUrl("/login") // login주소로 요청이 오면 시큐리티가 낚아채서 대신 로그인을 진행한다.
-			.defaultSuccessUrl("/");
+			.defaultSuccessUrl("/")
+			.and()
+			.oauth2Login()
+			.loginPage("/loginForm");
 	}
 }
